25fe267fe074bd1a4befd96eb1854e1c6b743822
SimonN <eiderdaus@gmail.com>
2016-02-17 09:29:25 +0100

    Hello World from several files

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from world import *
+
+def runMain():
+    world = World(3, 3)
+    print world._text
---
+++ src/world.py
@@ -0,0 +1,5 @@
+class World:
+    def __init__(self, xl, yl):
+        self._xl = xl
+        self._yl = yl
+        self._text = "Hello World"
