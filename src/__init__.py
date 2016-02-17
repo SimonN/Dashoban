735875e493ae319834abf103b455c7734335acc5
SimonN <eiderdaus@gmail.com>
2016-02-17 18:36:18 +0100

    moving on world

--- src/__init__.py
+++ src/__init__.py
@@ -4,3 +4,13 @@ from world import *
 
 def runMain():
     state = GameState(openLevel("./levels/tut.txt"))
+
+    state.draw()
+    state.movePlayer(0)
+    state.draw()
+    state.movePlayer(1)
+    state.draw()
+    state.movePlayer(2)
+    state.draw()
+    state.movePlayer(3)
+    state.draw()
--- src/state.py
+++ src/state.py
@@ -5,15 +5,9 @@ class GameState:
         self._world = world
         self._playerX = self._world.findPlayer()[0]
         self._playerY = self._world.findPlayer()[1]
-        self._world.draw()
-        print "player is at %d,%d" % (self._playerX, self._playerY)
-        self.movePlayer(0)
-        self.movePlayer(1)
-        self.movePlayer(2)
-        self.movePlayer(3)
 
     def movePlayer(self, direc):
-        assert direc >= 0 and direc < 4;
+        assert direc >= 0 and direc < 4
         assert self._world.get(self._playerX, self._playerY) == '@'
         plusX  = [0, 1, 0, -1][direc]
         plusY  = [-1, 0, 1, 0][direc]
@@ -21,3 +15,29 @@ class GameState:
                                  self._playerY +   plusY)
         behind = self._world.get(self._playerX + 2*plusX,
                                  self._playerY + 2*plusY)
+        if onto == ' ':
+            self._movePlayerLeaveSpace(plusX, plusY)
+        elif onto == '.':
+            self._collectDiamond(plusX, plusY)
+        elif onto in ['+', '-', '|'] and behind == ' ':
+            self._pushCrate(plusX, plusY)
+
+    def draw(self):
+        self._world.draw()
+
+    def _movePlayerLeaveSpace(self, plusX, plusY):
+        self._world.set(self._playerX, self._playerY, ' ')
+        self._playerX += plusX
+        self._playerY += plusY
+        self._world.set(self._playerX, self._playerY, '@')
+
+    def _collectDiamond(self, plusX, plusY):
+        self._movePlayerLeaveSpace(plusX, plusY)
+        if self._noDiamondsLeft:
+            pass
+
+    def _pushCrate(self, plusX, plusY):
+        pass
+
+    def _noDiamondsLeft(self):
+        pass
